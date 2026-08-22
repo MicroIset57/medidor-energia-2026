65693a34f44caa6bd6f3b2f879df108cb74f832b
Javier Rambaldo <jrambaldo@gmail.com>
2026-08-21 21:21:59 -0300

    agrego documentacion y esp basico

---
+++ esp32/main.cpp
@@ -0,0 +1,19 @@
+/*
+    ESP32-C3
+    Medidor de energía con ACS712
+    Muestra valores en display OLED SSD1306
+*/
+
+#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+void setup()
+{
+}
+
+void loop()
+{
+}
